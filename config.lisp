f891efe4007d95b388040ed08d48b2654088495d
Kevin Zheng <kevinz5000@gmail.com>
2024-08-21 01:50:58 +0000

    Use production server config

--- config.lisp
+++ config.lisp
@@ -1,8 +1,6 @@
 (defbuild "n14" :git "https://github.com/Vault-Overseers/nuclear-14.git" "master")
-(defbuild "ds14" :git "file:///home/kevinz/workspace/games/ds14" "ds14/master")
+(definstance "n14" :build "n14" :port 1212)
+(definstance "n14map" :build "n14" :data "n14" :port 1401)
 
-(definstance "n14-prod" :build "n14" :port 1401)
-(definstance "n14-map" :build "n14" :data "n14-prod" :port 1402)
-
-(definstance "ds14-prod" :build "ds14" :port 1212)
-(definstance "ds14-map" :build "ds14" :port 1213)
+(defbuild "ds14" :git "https://github.com/SS14-Classic/deep-station-14.git" "master")
+(definstance "ds14" :build "ds14" :port 1403)
